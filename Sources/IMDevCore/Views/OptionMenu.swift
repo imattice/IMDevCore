873284365c678c843d45cd0fa37d699ad13f0f22
Ike Mattice <ikemattice@icloud.com>
2023-02-26 11:33:33 -0800

    Feat: Update OptionMenuDataSource to conform to Labelable; Add ability to add a sort method to OptionMenu

--- Sources/IMDevCore/Views/OptionMenu.swift
+++ Sources/IMDevCore/Views/OptionMenu.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-/// Defines a object that can be used to drive a ``OptionMenu`` and conforms to both ``Hashable`` and ``CaseIterable`` protocols
-public typealias OptionMenuDataSource = Hashable & CaseIterable
+/// Defines a object that can be used to drive a ``OptionMenu`` and conforms to the ``Labelable``, ``Hashable`` and ``CaseIterable`` protocols
+public typealias OptionMenuDataSource = Hashable & CaseIterable & Labelable
 
 /// A View that organizes a ``OptionMenuDataSource`` into a ``ForEach`` and can be presented as a menu that can select options
 ///
@@ -26,10 +26,11 @@ public struct OptionMenu<T: OptionMenuDataSource, CellContent: View>: View {
     @Binding var selection: T?
     /// A function that creates the view used as the cells
     let cellBuilder: CellContentBuilder?
+    let sortAction: ((any OptionMenuDataSource, any OptionMenuDataSource) -> Bool)
 
     /// All options contained in the ``MenuDataSource``
     var options: [T] {
-        T.allCases.map { $0 }
+        T.allCases.map { $0 }.sorted(by: sortAction)
     }
 
     public var body: some View {
@@ -45,7 +46,7 @@ public struct OptionMenu<T: OptionMenuDataSource, CellContent: View>: View {
     @ViewBuilder
     private func cellContent(for option: T) -> some View {
         if let cellBuilder {
-            cellBuilder(cellTitle(for: option), selection == option)
+            cellBuilder(option.label, selection == option)
         } else {
             defaultCell(for: option)
         }
@@ -57,7 +58,7 @@ public struct OptionMenu<T: OptionMenuDataSource, CellContent: View>: View {
         HStack {
             Image(systemName: "checkmark")
                 .opacity(option == selection ? 1 : 0)
-            Text(cellTitle(for: option))
+            Text(option.label)
             Spacer()
         }
         .padding()
@@ -73,12 +74,6 @@ public struct OptionMenu<T: OptionMenuDataSource, CellContent: View>: View {
             selection = option
         }
     }
-
-    /// Converts the given option to a title cased label to use in the cell
-    /// - Parameter option: The option to consider
-    private func cellTitle(for option: T) -> String {
-        String(describing: option).titleCased
-    }
 }
 
 // MARK: - Init
@@ -86,20 +81,30 @@ extension OptionMenu {
     /// Creates a ``OptionMenu`` from the provided selection and a custom cell View
     /// - Parameters:
     ///   - selection: The selected option, if any
+    ///   - sortedBy: An optional sorting method to apply to the results, if any, with a default of no sorting applied
     ///   - cellContent: A ViewBuilder that creates the cell content.  Provides a String to be used as the cell title, and a Bool to determine the state of the cell if selected or unselected
     public init(
         selection: Binding<T?>,
-        cellContent: @escaping CellContentBuilder) {
+        sortedBy sortMethod: ((any OptionMenuDataSource, any OptionMenuDataSource) -> Bool)? = nil,
+        cellContent: @escaping CellContentBuilder
+    ) {
         self._selection = selection
         self.cellBuilder = cellContent
+        self.sortAction = sortMethod ?? { _, _ in return true }
     }
 }
 
 extension OptionMenu where CellContent == EmptyView {
     /// Creates a ``OptionMenu`` from the provided selection using the default cell ViewBuilder
-    /// - Parameter selection: The selected option, if any
-    public init(selection: Binding<T?>) {
+    /// - Parameters:
+    ///  - selection: The selected option, if any
+    ///  - sortedBy: An optional sorting method to apply to the results, if any, with a default of no sorting applied
+    public init(
+        selection: Binding<T?>,
+        sortedBy sortMethod: ((any OptionMenuDataSource, any OptionMenuDataSource) -> Bool)? = nil
+    ) {
         self._selection = selection
+        self.sortAction = sortMethod ?? { _, _ in return true }
         self.cellBuilder = nil
     }
 }
@@ -109,24 +114,18 @@ struct OptionMenu_Previews: PreviewProvider {
     @State static var selectedOption: Option? = .oneFinalOption
 
     static var previews: some View {
-        defaultCellInList
-
-        customCellInList
-    }
-}
-
-// MARK: Preview View Builders
-extension OptionMenu_Previews {
-    @ViewBuilder
-    static var defaultCellInList: some View {
         List {
             OptionMenu(selection: $selectedOption)
         }
         .previewDisplayName("Default Cell in List")
-    }
 
-    @ViewBuilder
-    static var customCellInList: some View {
+        List {
+            OptionMenu(
+                selection: $selectedOption,
+                sortedBy: { $0.label < $1.label })
+        }
+        .previewDisplayName("Alphabetical Sorted Menu in List")
+
         List {
             OptionMenu(selection: $selectedOption) { cellTitle, isSelected in
                 customCell(titled: cellTitle, isSelected: isSelected)
@@ -134,7 +133,10 @@ extension OptionMenu_Previews {
         }
         .previewDisplayName("Custom Cell in List")
     }
+}
 
+// MARK: Preview View Builders
+extension OptionMenu_Previews {
     @ViewBuilder
     static func customCell(titled title: String, isSelected: Bool) -> some View {
         Text(title)
@@ -145,9 +147,13 @@ extension OptionMenu_Previews {
 
 // MARK: Preview Options
 extension OptionMenu_Previews {
-    enum Option: CaseIterable {
+    enum Option: CaseIterable, Labelable {
         case optionOne
         case another
         case oneFinalOption
+
+        var label: String {
+            String(describing: self).titleCased
+        }
     }
 }
