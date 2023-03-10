f6219cf14f6c87b80aa2a330c50e89b1ae132951
Ike Mattice <ikemattice@icloud.com>
2023-03-09 19:51:43 -0800

    Feat: Add DataInitializer

---
+++ Sources/IMDevCore/DataManagement/DataInitializer.swift
@@ -0,0 +1,31 @@
+//
+//  DataInitializer.swift
+//  
+//
+//  Created by Ike Mattice on 3/9/23.
+//
+
+import Foundation
+
+/// An object used to load data into Core Data
+public class DataInitializer {
+    private static let databaseInitializationKey: String = "hasInitializedDatabase"
+
+    /// Indicates if the data has already been initialized
+    static public var hasInitializedData: Bool {
+        UserDefaults.standard.bool(forKey: databaseInitializationKey)
+    }
+
+    /// Destroys the current store and replaces it with a  new store, effectively wiping all data from the persistent stores
+    /// - Parameter dataStack: The data stack to clear
+    public func clearData(from dataStack: CoreDataStack) {
+        do {
+            guard let url = dataStack.persistentContainer.persistentStoreDescriptions.first?.url else { return }
+            try dataStack.persistentContainer.persistentStoreCoordinator.destroyPersistentStore(at: url, type: .sqlite)
+            _ = try dataStack.persistentContainer.persistentStoreCoordinator.addPersistentStore(type: .sqlite, at: url)
+
+        } catch {
+            print(error)
+        }
+    }
+}
