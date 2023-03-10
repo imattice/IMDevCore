d2e9fde471a43c31956f69dba61a2d3b25b61980
Ike Mattice <ikemattice@icloud.com>
2023-03-09 20:18:16 -0800

    Feat: limit scope of Loadable to only load Self from the file name

--- Sources/IMDevCore/DataManagement/Loadable.swift
+++ Sources/IMDevCore/DataManagement/Loadable.swift
@@ -7,42 +7,17 @@
 
 import Foundation
 
-/// Describes an object that can be loaded from a ``Codable`` response object
-public protocol Loadable {
-    /// Defines the type that models the json response object
-    associatedtype Response: Codable
+/// Describes an ``Codable`` object that can be loaded from a local file
+public protocol Loadable: Codable {
     /// A file that contains the json source object
     static var fileName: String { get }
-
-    /// An initializer that maps the values from the response object to the Loadable object
-    /// - Parameter response: The response object to create the object from
-    init(from response: Response)
-
-    /// A function that loads all objects into a collection
-    /// - Returns: The collection of objects
-    static func loadAll() -> [Self]
 }
 
 extension Loadable {
-    /// A function that loads all objects into a collection
-    /// - Returns: The collection of objects
-    public static func loadAll() -> [Self] {
-        var responses: [Response] = [ ]
-
-        do {
-            responses = try loadResponses()
-        } catch {
-            print("Failed to parse response")
-            print(error)
-        }
-
-        return responses.map { Self(from: $0) }
-    }
-
     /// Loads all responses from the file name
     /// - Parameter fileName: The filename where the json object is located
     /// - Returns: A collection of responses created from the json file
-    public static func loadResponses() throws -> [Response] {
+    public static func loadAll() throws -> [Self] {
         guard let path = Bundle.main.path(forResource: fileName, ofType: "json") else {
             print(fileName)
             throw ParseError.fileNotFound
@@ -50,7 +25,7 @@ extension Loadable {
 
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
-            let decoded = try JSONDecoder().decode([Response].self, from: data)
+            let decoded = try JSONDecoder().decode([Self].self, from: data)
 
             return decoded
         } catch {
