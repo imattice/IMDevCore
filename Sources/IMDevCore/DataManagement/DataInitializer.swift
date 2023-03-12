05d4b870055faa53f2727b8a0d9f2bab034511fa
Ike Mattice <ikemattice@icloud.com>
2023-03-11 21:08:27 -0800

    Feat: Add completeInitialization method to DataInitializer

--- Sources/IMDevCore/DataManagement/DataInitializer.swift
+++ Sources/IMDevCore/DataManagement/DataInitializer.swift
@@ -19,6 +19,11 @@ public class DataInitializer {
     /// Creates a new ``DataInitializer``
     public init() { /* Empty public initializer */ }
 
+    /// Runs the finalization methods after initialization has completed
+    public func completeInitialization() {
+        UserDefaults.standard.setValue(true, forKey: DataInitializer.databaseInitializationKey)
+    }
+
     /// Destroys the current store and replaces it with a  new store, effectively wiping all data from the persistent stores
     /// - Parameter dataStack: The data stack to clear
     public func clearData(from dataStack: CoreDataStack) {
