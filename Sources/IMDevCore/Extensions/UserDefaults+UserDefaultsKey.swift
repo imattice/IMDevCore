90b35347cbce63783d03a11bc8a1b503fcae323d
Ike Mattice <ikemattice@icloud.com>
2023-06-23 22:07:49 -0700

    Add UserDefaults extension to use UserDefaultsKey

---
+++ Sources/IMDevCore/Extensions/UserDefaults+UserDefaultsKey.swift
@@ -0,0 +1,31 @@
+//
+//  UserDefaults+UserDefaultsKey.swift
+//  
+//
+//  Created by Ike Mattice on 6/23/23.
+//
+
+import Foundation
+
+extension UserDefaults {
+    /// A convenience method that wraps ``UserDefaults.standard.set(_value:forKey:)`` method to allow direct injection of a ``UserDefaultsKey``
+    /// - Parameters:
+    ///   - value: The value to persist to UserDefaults
+    ///   - userDefaultsKey: The key to set the value to
+    func set(_ value: Any?, forKey userDefaultsKey: some UserDefaultsKey) {
+        self.set(value, forKey: userDefaultsKey.key)
+    }
+
+    /// A convenience method that wraps ``UserDefaults.standard.value(forKey:)`` method to allow direct injection of a ``UserDefaultsKey``
+    /// - Parameter userDefaultsKey: The key to access the value from
+    /// - Returns: The value for the associated key, if any
+    func value(forKey userDefaultsKey: some UserDefaultsKey) -> Any? {
+        self.value(forKey: userDefaultsKey.key)
+    }
+
+    /// A convenience method that wraps ``UserDefaults.standard.removeObject(forKey:)`` method to allow direct injection of a ``UserDefaultsKey``
+    /// - Parameter userDefaultsKey: The key to remove the value from
+    func removeObject(forKey userDefaultsKey: some UserDefaultsKey) {
+        self.removeObject(forKey: userDefaultsKey.key)
+    }
+}
